31654903fe8498b7f85c7c9cdf73386959797469
L <l@cleveritgroup.com>
2020-11-17 22:09:23 -0300

    header

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -7,59 +7,68 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Apoyamos a los restaurantes del país para adaptarse a la nueva normalidad gracias a la tecnología."
+        ></meta>
+        <meta name="url" content="https://www.fodture.com/"></meta>
+        <meta
+          name="image"
+          content="https://cleveritimages.blob.core.windows.net/images/Fodture-rrss.png"
+        ></meta>
+        <meta property="og:type" content="product"></meta>
+        <meta property="og:url" content="https://www.fodture.com/"></meta>
+        <meta
+          property="og:title"
+          content="Fodture - Restaurantes del futuro"
+        ></meta>
+        <meta
+          property="og:description"
+          content="Apoyamos a los restaurantes del país para adaptarse a la nueva normalidad gracias a la tecnología."
+        ></meta>
+        <meta
+          property="og:image"
+          content="https://cleveritimages.blob.core.windows.net/images/Fodture-rrss.png"
+        ></meta>
+        <meta property="og:image:width" content="1200"></meta>
+        <meta property="og:image:height" content="628"></meta>
+        <meta property="twitter:card" content="summary_large_image"></meta>
+        <meta property="twitter:url" content="https://www.fodture.com/"></meta>
+        <meta
+          property="twitter:title"
+          content="Fodture - Restaurantes del futuro "
+        ></meta>
+        <meta
+          property="twitter:description"
+          content="Apoyamos a los restaurantes del país para adaptarse a la nueva normalidad gracias a la tecnología."
+        ></meta>
+        <meta
+          property="twitter:image"
+          content="https://cleveritimages.blob.core.windows.net/images/Fodture-rrss.png"
+        ></meta>
+        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta name="twitter:url" content="https://www.fodture.com/"></meta>
+        <meta
+          name="twitter:title"
+          content="Fodture - Restaurantes del futuro "
+        ></meta>
+        <meta
+          name="twitter:description"
+          content="Apoyamos a los restaurantes del país para adaptarse a la nueva normalidad gracias a la tecnología."
+        ></meta>
+        <meta
+          name="twitter:image"
+          content="https://cleveritimages.blob.core.windows.net/images/Fodture-rrss.png"
+        ></meta>
+        <link
+          rel="image_src"
+          href="https://cleveritimages.blob.core.windows.net/images/Fodture-rrss.png"
+        ></link>
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1 className={styles.title}>Header Test</h1>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
